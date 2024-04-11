e5401d209cc0908d57c6f671ff975be12f37bbb8
tmszny <tomasz.matuszny@gmail.com>
2024-04-11 17:54:28 +0200

    first commit

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,42 @@
+package org.example;
+
+import java.io.IOException;
+import java.util.Scanner;
+
+public class Main {
+    public static volatile boolean keepRunning = true;
+
+    public static void main(String[] args) throws IOException, InterruptedException {
+        System.out.println("Press enter to stop");
+
+        Thread t = new Thread(() -> {
+            StringBuilder builder = new StringBuilder();
+
+            while (keepRunning) {
+                builder.append("*");
+                String firstLine = builder.toString();
+                builder.append("**");
+                String secondLine = builder.toString();
+                builder.setLength(0);
+                builder.append(firstLine);
+
+
+                System.out.println(firstLine);
+                System.out.println(secondLine);
+
+                try {
+                    Thread.sleep(500);
+                } catch (InterruptedException e) {
+                    throw new RuntimeException(e);
+                }
+            }
+        });
+
+        t.start();
+
+        //stop
+        System.in.read();
+        keepRunning = false;
+        t.join();
+    }
+}
